3661465781deba4c7dc828e596c2f9c277daf172
Istvan <olivaistvan@gmail.com>
2022-08-28 13:19:28 -0300

    feat: (models) add matchModel

---
+++ app/backend/src/database/models/matchModel.ts
@@ -0,0 +1,57 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Team from './teamModel';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: number;
+}
+
+Match.init(
+  {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeam: {
+      type: INTEGER,
+      allowNull: false,
+      field: 'home_team',
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+      field: 'home_team_goals',
+    },
+    awayTeam: {
+      type: INTEGER,
+      allowNull: false,
+      field: 'away_team',
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+      field: 'away_team_goals',
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+      field: 'in_progress',
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'awayTeam' });
+
+export default Match;
