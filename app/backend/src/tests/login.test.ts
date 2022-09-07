dd30b7594b02aaadc01aee7070b3e421130c0d1b
Istvan <olivaistvan@gmail.com>
2022-09-06 23:10:05 -0300

    tests: implements POST / login test

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,50 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+
+import { userReq, userRes } from './mock/users';
+import User from '../database/models/userModel';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Test endpoint POST/login', () => {
+
+  describe('In case of successful request', () => {
+
+    beforeEach(async () => {
+      sinon
+        .stub(User, 'findOne')
+        .resolves(userRes as User)
+    });
+
+    afterEach(() => sinon.restore());
+
+    it('Should return status 200', async () => {
+      const response = await chai.request(app).post('/login').send({
+        email: userReq.email,
+        password: userReq.password,
+      });
+
+      expect(response.status).to.equal(200);
+    });
+
+    it('Should return a token', async () => {
+      const response = await chai.request(app).post('/login').send({
+        email: userReq.email,
+        password: userReq.password,
+      });
+
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.have.property('token');
+      expect(response.body.token).to.be.a('string');
+    });
+  })
+});
