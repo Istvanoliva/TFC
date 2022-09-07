ddc3f46e18c88456deb66d31a1261946c093d1ad
Istvan <olivaistvan@gmail.com>
2022-09-07 19:33:58 -0300

    feat: (routers) add teamsRouter to index

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import userRouter from './users';
+import teamsRouter from './teams';
 
 const router = Router();
 
 router.use(userRouter);
+
+router.use(teamsRouter);
 
 export default router;
