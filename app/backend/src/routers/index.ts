f5ca57c0b218b9fd8ed7ec527ea1af50ec8fc78d
Istvan <olivaistvan@gmail.com>
2022-09-08 17:06:48 -0300

    feat: (routers) add matchesRouter to index

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import userRouter from './users';
 import teamsRouter from './teams';
+import matchesRouter from './matches';
 
 const router = Router();
 
 router.use(userRouter);
 
 router.use(teamsRouter);
+
+router.use(matchesRouter);
 
 export default router;
