a1b994d0d6eb890dbe8c1acf4a4bee79674ef673
Istvan <olivaistvan@gmail.com>
2022-09-08 16:59:49 -0300

    feat: (test/mock) create matches mock

---
+++ app/backend/src/tests/mock/matches.ts
@@ -0,0 +1,44 @@
+export const matchesMock = [
+    {
+      id: 1,
+      homeTeam: 16,
+      homeTeamGoals: 1,
+      awayTeam: 8,
+      awayTeamGoals: 1,
+      inProgress: false,
+      teamHome: {
+        teamName: 'São Paulo'
+      },
+      teamAway: {
+        teamName: 'Grêmio'
+      },
+    },
+    {
+      id: 2,
+      homeTeam: 3,
+      homeTeamGoals: 0,
+      awayTeam: 2,
+      awayTeamGoals: 3,
+      inProgress: false,
+      teamHome: {
+        teamName: 'Botafogo'
+      },
+      teamAway: {
+        teamName: 'Bahia'
+      },
+    },
+    {
+      id: 41,
+      homeTeam: 16,
+      homeTeamGoals: 2,
+      awayTeam: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+      teamHome: {
+        teamName: 'São Paulo'
+      },
+      teamAway: {
+        teamName: 'Internacional'
+      },
+    },
+  ];
