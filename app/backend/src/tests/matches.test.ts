36dbf1bda4ef05aab48bad14781d8100d2fc00fe
Istvan <olivaistvan@gmail.com>
2022-09-09 20:53:27 -0300

    tests: (matches) add new tests GET /matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -6,23 +6,26 @@ import { app } from '../app';
 
 import Match from '../database/models/matchModel';
 import IMatch from '../typescript/interfaces/matchesInterface';
-import { matchesMock } from './mock/matches';
+import { matchesMock, inProgress, finished } from './mock/matches';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Test endpoint GET/matches', () => {
+describe('Test endpoint GET /matches', () => {
 
   describe('In case of successful request', () => {
 
-    beforeEach(async () => {
-      sinon
-        .stub(Match, 'findAll')
-        .resolves(matchesMock as IMatch[])
+    before(async () => {
+      const stubResolve = sinon.stub(Match, 'findAll');
+
+      stubResolve.onCall(0).resolves(matchesMock as IMatch[])
+      stubResolve.onCall(1).resolves(matchesMock as IMatch[])
+      stubResolve.onCall(2).resolves(inProgress as IMatch[]);
+      stubResolve.onCall(3).resolves(finished as IMatch[]);
     });
 
-    afterEach(() => sinon.restore());
+    after(() => sinon.restore());
     
     it('Should return status 200', async () => {
         const response = await chai.request(app).get('/matches').send();
@@ -32,6 +35,16 @@ describe('Test endpoint GET/matches', () => {
     it('Should return an array with all matches listed', async () => {
         const response = await chai.request(app).get('/matches').send();
         expect(response.body).to.be.deep.equal(matchesMock);
+    });
+
+    it('When search by "in progress" matches', async () => {
+      const response = await chai.request(app).get('/matches?inProgress=true');
+      expect(response.body).to.be.deep.equal(inProgress);
+    });
+
+    it('When search by "finished" matches', async () => {
+      const response = await chai.request(app).get('/matches?inProgress=false');
+      expect(response.body).to.be.deep.equal(finished);
     });
   });
 })
