a247f2a1fdc4e970bec0f3a306090220a8e9e7ba
Istvan <olivaistvan@gmail.com>
2022-09-09 00:10:38 -0300

    refactor: (teamService)

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,12 +1,11 @@
 import { Identifier } from 'sequelize/types';
 import HttpError from '../helpers/HttpError';
 import Team from '../database/models/teamModel';
-import { teamsList, team } from '../typescript/interfaces/teamInterface';
 
 class TeamService {
-  getTeams = async (): Promise<teamsList> => Team.findAll();
+  getTeams = async (): Promise<Team[]> => Team.findAll();
 
-  getTeam = async (id: Identifier): Promise<team> => {
+  getTeam = async (id: Identifier): Promise<Team> => {
     const findTeam = await Team.findByPk(id);
 
     if (!findTeam) throw new HttpError(404, 'Team does not exist');
