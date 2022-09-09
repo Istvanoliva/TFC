6ee9b133a1e465f60c9a70c91c359ea8b11566a5
Istvan <olivaistvan@gmail.com>
2022-09-09 20:57:49 -0300

    feat: (MatchService) add getProgressMatchs()

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -2,10 +2,16 @@ import Match from '../database/models/matchModel';
 import Team from '../database/models/teamModel';
 
 class MatchService {
-  getAllMatches = async () => Match.findAll({ include: [
+  getAllMatches = async (): Promise<Match[]> => Match.findAll({ include: [
     { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
     { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
   ] });
+
+  getProgressMatchs = async (inProgress: boolean): Promise<Match[]> => Match.findAll({ include: [
+    { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+    { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+  ],
+  where: { inProgress } });
 }
 
 export default MatchService;
