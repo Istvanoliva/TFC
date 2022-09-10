ee22919dfc99a7bc7cd0605a871394014ee62b97
Istvan <olivaistvan@gmail.com>
2022-09-10 14:48:40 -0300

    feat: (MatchService) create creteMatch()

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,7 +1,15 @@
 import Match from '../database/models/matchModel';
 import Team from '../database/models/teamModel';
+import TeamService from './TeamService';
+import { ICreateMatch } from '../typescript/interfaces/matchesInterface';
 
 class MatchService {
+  teamService: TeamService;
+
+  constructor() {
+    this.teamService = new TeamService();
+  }
+
   getAllMatches = async (): Promise<Match[]> => Match.findAll({ include: [
     { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
     { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
@@ -12,6 +20,16 @@ class MatchService {
     { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
   ],
   where: { inProgress } });
+
+  createMatch = async (newMatch: ICreateMatch): Promise<Match> => {
+    const { homeTeam, awayTeam } = newMatch;
+
+    await this.teamService.validateTeam(homeTeam);
+    await this.teamService.validateTeam(awayTeam);
+
+    const createMatch = await Match.create({ ...newMatch, inProgress: true });
+    return createMatch;
+  };
 }
 
 export default MatchService;
