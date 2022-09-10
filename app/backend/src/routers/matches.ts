dc8b1aa5895c397fb78daf8cefa0d28063eb5a7b
Istvan <olivaistvan@gmail.com>
2022-09-10 14:49:12 -0300

    feat: (routers) create POST /matches

--- app/backend/src/routers/matches.ts
+++ app/backend/src/routers/matches.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import TokenMiddleware from '../middlewares/TokenMiddleware';
 import MatchController from '../controllers/MatchController';
+import ValidateMatch from '../middlewares/validators/ValidateMatch';
 
 const matchesRouter = Router();
 
 const matches = new MatchController();
+const token = new TokenMiddleware();
+const validate = new ValidateMatch();
 
 matchesRouter.get('/matches', (req, res, next) => matches.getAllMatches(req, res, next));
+
+matchesRouter.post(
+  '/matches',
+  token.verify,
+  validate.checkTeams,
+  (req, res, next) => matches.createMatch(req, res, next),
+);
 
 export default matchesRouter;
