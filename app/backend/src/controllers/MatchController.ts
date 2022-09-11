fa896d15d3fb90eb5c2f47da990c2e2a7c470953
Istvan <olivaistvan@gmail.com>
2022-09-11 20:40:27 -0300

    feat: (Matchcontroller) add finishMatch()

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -34,6 +34,16 @@ class MatchController {
       next(error);
     }
   }
+
+  async finishMatch(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const finished = await this.service.finishMatch(Number(id));
+      res.status(200).json(finished);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default MatchController;
