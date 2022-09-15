8b3bdcb47002bc6249defdbf8d846f4dd4155a0a
Istvan <olivaistvan@gmail.com>
2022-09-15 12:47:11 -0300

    feat: (controllers) create class BoardController

---
+++ app/backend/src/controllers/BoardController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import BoardService from '../services/BoardService';
+
+class BoardController {
+  private service: BoardService;
+
+  constructor() {
+    this.service = new BoardService();
+  }
+
+  async getHomeBoards(_req: Request, res: Response, next:NextFunction) {
+    try {
+      const boards = await this.service.getBoards('home');
+      return res.status(200).json(boards);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default BoardController;
