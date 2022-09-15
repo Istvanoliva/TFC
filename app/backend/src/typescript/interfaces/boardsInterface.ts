5293b55fd52553e3d731f11c666cad40a2b1da1b
Istvan <olivaistvan@gmail.com>
2022-09-15 12:45:10 -0300

    feat: (interfaces) create boardsInterface

---
+++ app/backend/src/typescript/interfaces/boardsInterface.ts
@@ -0,0 +1,14 @@
+export interface IBoard {
+  name?: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number,
+}
+
+export type TArena = 'home' | 'away';
