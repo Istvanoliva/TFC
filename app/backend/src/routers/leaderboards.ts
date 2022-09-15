e733ced29abf0816c827bba8e8b2eab3f9322ca4
Istvan <olivaistvan@gmail.com>
2022-09-15 12:53:31 -0300

    feat: (routers) create GET /leaderboard/away

--- app/backend/src/routers/leaderboards.ts
+++ app/backend/src/routers/leaderboards.ts
@@ -7,4 +7,6 @@ const board = new BoardController();
 
 boardsRouter.get('/leaderboard/home', (req, res, next) => board.getHomeBoards(req, res, next));
 
+boardsRouter.get('/leaderboard/away', (req, res, next) => board.getAwayBoards(req, res, next));
+
 export default boardsRouter;
