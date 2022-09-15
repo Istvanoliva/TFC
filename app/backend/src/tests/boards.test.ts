1987fd393d74e7e01df60da39e397e1240a79fed
Istvan <olivaistvan@gmail.com>
2022-09-15 12:31:23 -0300

    tests: (boards) create GET /leaderboard/away test

--- app/backend/src/tests/boards.test.ts
+++ app/backend/src/tests/boards.test.ts
@@ -54,3 +54,42 @@ describe('Test endpoint GET/leaderboard/home', () => {
       });
     });
   })
+
+  describe('Test endpoint GET/leaderboard/away', () => {
+
+    describe('In case of successful request', () => {
+  
+      beforeEach(async () => {
+        sinon
+          .stub(Team, 'findAll')
+          .resolves(teams as Team[]);
+        sinon
+          .stub(Match, 'findAll')
+          .resolves(finished as IMatch[]);
+      });
+  
+      afterEach(() => sinon.restore());
+      
+      it('Should return status 200', async () => {
+          const response = await chai.request(app).get('/leaderboard/away');    
+          expect(response.status).to.equal(200);
+      });
+  
+      it('Should return an array with all away boards listed', async () => {
+          const response = await chai.request(app).get('/leaderboard/away');
+
+          expect(response.body).to.be.an('array');
+          expect(response.body[0]).to.be.an('object');
+          expect(response.body[0]).to.have.property('name');
+          expect(response.body[0]).to.have.property('totalPoints');
+          expect(response.body[0]).to.have.property('totalGames');
+          expect(response.body[0]).to.have.property('totalVictories');
+          expect(response.body[0]).to.have.property('totalDraws');
+          expect(response.body[0]).to.have.property('totalLosses');
+          expect(response.body[0]).to.have.property('goalsFavor');
+          expect(response.body[0]).to.have.property('goalsOwn');
+          expect(response.body[0]).to.have.property('goalsBalance');
+          expect(response.body[0]).to.have.property('efficiency');
+      });
+    });
+  })
